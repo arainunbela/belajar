479668c1f6ba4089feff523c8173d02e2fd8059a
arainunbela <71245039+arainunbela@users.noreply.github.com>
2020-09-17 14:59:32 +0700

    ainun.java

---
+++ praktikum/ainun.java
@@ -0,0 +1,8 @@
+public class ainun {
+
+      public static void main (String[] args){
+        System.out.println("Nama 	: Ar Ainun Bela"); // memunculkan Namaku : Ar Ainun Bela
+        System.out.println("NIM	: 6706200038"); // memunculkan NIM	: 6706200038
+        System.out.println("Kelas 	: D3IF 44-03"); // memunculkan Kelas 	: D3IF 44-03
+    }
+}
